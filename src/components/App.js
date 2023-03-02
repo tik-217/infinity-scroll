44b8b904ab4af1140dbb3e7733c3e303c77be7e5
Tigran Gabulyan <70902232+tik-217@users.noreply.github.com>
2023-03-03 02:35:24 +0300

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,124 @@
+import { React, useEffect, useState, useRef, useCallback } from "react";
+
+/** components */
+import useGetUsers from "../hooks/useGetUsers";
+
+/** antd - UI */
+import { Avatar, Card } from "antd";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+  LoadingOutlined,
+} from "@ant-design/icons";
+
+import "./App.css";
+
+const { Meta } = Card;
+
+/**
+ * @description The main component of the output of user cards
+ * @component App
+ */
+export default function App() {
+  /**
+   * Download status
+   * @constant {boolean} */
+  const [loading, setLoading] = useState(true);
+
+  /**
+   * List of users
+   * @typedef { import("../types").IUsersResults} IUsersResults
+   * @type {IUsersResults[]}
+   * */
+  const [allUsers, setAllUsers] = useState([]);
+
+  /**
+   * Page number of the current card page with users
+   * @constant {number}
+   * */
+  const [page, setPage] = useState(1);
+
+  /**
+   * Server response with users
+   * @typedef { import("../types").IUsersResults} IUsersResults
+   * */
+  const { users } = useGetUsers(page);
+
+  const observable = useRef();
+
+  /**
+   * @description The function finds the last card in the viewport of the user window
+   * */
+  const triggerLoad = useCallback(
+    /** @param { HTMLDivElement } node HTML tag of the element on which this function is triggered */
+    (node) => {
+      if (loading) return;
+      if (observable.current) observable.current.disconnect();
+
+      /** Assign a new page if the list is scrolled to the end */
+      observable.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setPage((page) => page + 1);
+        }
+      });
+
+      if (node) observable.current.observe(node);
+    },
+    // eslint-disable-next-line
+    [loading]
+  );
+
+  useEffect(() => {
+    /** The user's upload status. Hide the download if the cards have loaded */
+    if (users.length === 0) {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+
+    if (users.length === 0) return;
+
+    /** Forming an array of cards with users. The previous cards and those received from the server are used */
+    setAllUsers((allUsers) => [...allUsers, ...users]);
+  }, [users]);
+
+  return (
+    <>
+      {loading && (
+        <div className="cards__spiner">
+          <LoadingOutlined
+            style={{
+              fontSize: 24,
+            }}
+            spin
+          />
+        </div>
+      )}
+      <div className="cards">
+        {allUsers.map((el, i) => {
+          return (
+            <Card
+              style={{
+                width: 300,
+              }}
+              actions={[
+                <SettingOutlined key="setting" />,
+                <EditOutlined key="edit" />,
+                <EllipsisOutlined key="ellipsis" />,
+              ]}
+              key={i}
+            >
+              {(i + 1) % 10 === 0 && <div ref={triggerLoad}></div>}
+              <Meta
+                avatar={<Avatar src={el.picture.thumbnail} />}
+                title={`${el.name.first} ${el.name.last}`}
+                description={el.email}
+              />
+            </Card>
+          );
+        })}
+      </div>
+    </>
+  );
+}
---
+++ src/hooks/useGetUsers.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+/**
+ * @typedef { import("../types").IUsersApi} IUsersApi
+ * @typedef { import("../types").IUsersResults} IUsersResults
+ *
+ * @description A hook for getting user cards
+ * @param {number} page
+ * @returns {{IUsersResults}}
+ */
+export default function useGetUsers(page) {
+  /**
+   * Array of users
+   * @type {IUsersResults[][]}
+   * */
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function usersList() {
+      /**
+       * @description Request to receive users
+       * @type {IUsersApi}
+       * */
+      const response = await fetch(
+        `https://randomuser.me/api/?page=${page}&inc=name,email,picture&results=20&seed=abc`
+      )
+        .then((res) => res.json())
+        .then(
+          /** @param {IUsersApi} data */
+          (data) => data
+        )
+        .catch((err) => console.log(err));
+
+      setUsers(response.results);
+    }
+
+    usersList();
+  }, [page]);
+
+  return {
+    users,
+  };
+}
