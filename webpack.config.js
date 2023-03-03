13dd19aa7ff5c587bf7234bbf9086860e5555f05
Tigran Gabulyan <70902232+tik-217@users.noreply.github.com>
2023-03-03 14:05:38 +0300

    Change folder name

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, "./dist"),
+      directory: path.resolve(__dirname, "./public"),
     },
     compress: true,
     port: 9000,
